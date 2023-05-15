e9a5c0eac0835d25da9c3eb2588c14f23b7e65d2
[ Ankit Fukte] <[ankitfukte11@gmail.com]>
2023-05-15 16:25:16 +0530

    adding bracker

--- Client/src/pages/MyleadTable.jsx
+++ Client/src/pages/MyleadTable.jsx
@@ -414,7 +414,6 @@ const MyleadTable = ({ loandetail,length }) => {
                   </div>
                   <div className="border-t-2 border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-neutral-50"></div>
                 </div>
-              </div>
             );
           })}
       </div>
