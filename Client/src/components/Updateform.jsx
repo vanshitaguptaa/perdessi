7d2f33bb31d965e185467f35d4a55b5859b8484f
vikasAjiledone <vikas.pandey@ajiledone.com>
2023-05-20 12:47:46 +0530

    update

--- Client/src/components/Updateform.jsx
+++ Client/src/components/Updateform.jsx
@@ -1,52 +1,14 @@
 import React, { useEffect } from "react";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
-const schema = yup
-  .object()
-  .shape({
-    first_name: yup.string().min(2).required(),
-    age: yup.number().positive().integer().required(),
-    last_name: yup.string().required(),
-    password: yup.string().optional(),
-    middlename: yup.string().optional(),
-    email: yup.string().email().required(),
-    reportto: yup.string().required(),
-    address: yup.string().required(),
-    number: yup.number().min(10).required(),
-    EmergencyContactNumber: yup.number().min(10).required(),
-    adharno: yup.number().required(),
-    role: yup.string().required(),
-    gender: yup.string().required(),
-    nationality: yup.string().required(),
-    religion: yup.string().required(),
-    martialStatus: yup.string().required(),
-    emergencyContactName: yup.string().required(),
-    emergencyContactRelationship: yup.string().required(),
-    BankName: yup.string().required(),
-    BranchName: yup.string().required(),
-    IFSCcode: yup.string().required(),
-    ACholdername: yup.string().required(),
-    BankAccNo: yup.number().required(),
-    joiningdate: yup.date().required(),
-    dob: yup.date().required(),
-    PanNo: yup.string().required(),
-}).required();
+
 
 const Updateform = ({Profiledata}) => {
   const navigate = useNavigate();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
+ 
 
   console.log(Profiledata);
   const response= (data) =>{
@@ -61,22 +23,30 @@ const Updateform = ({Profiledata}) => {
     }
   }
 
-  const onSubmit = (data) => {
-    console.log(data)
-    console.log("client");
-    fetch("http://localhost:5000/api/v1/crm/updateempolyee/:id", {
-      method: "PATCH",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((response) => response.json())
-      .then((json) => { response(json)});
-  };
+  // const handleUpdate =  (data) => {
+  //   data.preventDefault();
+  //   console.log("handleUpdate")
+  //   console.log(data)
+  //   console.log("client");
+  //   fetch("http://localhost:5000/api/v1/crm/updateempolyee/:id", {
+  //     method: "PATCH",
+  //     body: JSON.stringify(data),
+  //     headers: {
+  //       "Content-type": "application/json; charset=UTF-8",
+  //     },
+  //   })
+  //     .then((response) => response.json())
+  //     .then((res) => console.log(res.data));
+  // };
+
+  const handleUpdate = (e)=>{
+    e.preventDefault();
+    console.log("handleUpdate")
+  }
+
   return (
     <>
-      <form className="w-full mt-12" onSubmit={handleSubmit(onSubmit)}>
+      <form className="w-full mt-12" onSubmit={handleUpdate}>
         <div className="flex flex-wrap mx-3 mb-6 justify-center items-center">
           <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
             <label
@@ -91,9 +61,8 @@ const Updateform = ({Profiledata}) => {
               type="text"
               placeholder=""
               defaultValue={Profiledata.first_name}
-              {...register("first_name")}
+            
             />
-            <small className="text-red-600">{errors.first_name?.message}</small>
           </div>
           <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
             <label
@@ -108,9 +77,7 @@ const Updateform = ({Profiledata}) => {
               type="text"
               placeholder=""
               defaultValue={Profiledata.first_name}
-              {...register("middlename")}
             />
-             <small className="text-red-600">{errors.middlename?.message}</small>
           </div>
           <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
             <label
@@ -125,9 +92,7 @@ const Updateform = ({Profiledata}) => {
               type="text"
               placeholder=""
               defaultValue={Profiledata.last_name}
-              {...register("last_name")}
             />
-            <small className="text-red-600">{errors.last_name?.message}</small>
           </div>
           <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
             <label
@@ -143,9 +108,7 @@ const Updateform = ({Profiledata}) => {
               placeholder=""
               defaultValue={Profiledata.adharno}
               readOnly
-              {...register("adharno")}
             />
-            <small className="text-red-600">{errors.adharno?.message}</small>
           </div>
         </div>
         <div className="flex flex-wrap mx-3 mb-6 justify-center items-center">
@@ -162,9 +125,7 @@ const Updateform = ({Profiledata}) => {
               type="email"
               placeholder=""
               defaultValue={Profiledata.email}
-              {...register("email")}
             />
-            <small className="text-red-600">{errors.email?.message}</small>
           </div>
           <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
             <label
@@ -179,9 +140,7 @@ const Updateform = ({Profiledata}) => {
               type="text"
               placeholder=""
               defaultValue={Profiledata.address}
-              {...register("address")}
-            />
-            <small className="text-red-600">{errors.address?.message}</small>
+                        />
           </div>
           <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
             <label
@@ -197,9 +156,8 @@ const Updateform = ({Profiledata}) => {
               placeholder=""
               defaultValue={Profiledata.reportto}
               readOnly
-              {...register("reportto")}
+            
             />
-            <small className="text-red-600">{errors.reportto?.message}</small>
           </div>
           <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
             <label
@@ -214,9 +172,8 @@ const Updateform = ({Profiledata}) => {
               type="number"
               placeholder=""
               defaultValue={Profiledata.number}
-              {...register("number")}
+             
             />
-            <small className="text-red-600">{errors.number?.message}</small>
           </div>
         </div>
         <div className="flex flex-wrap mx-3 mb-2">
@@ -234,9 +191,8 @@ const Updateform = ({Profiledata}) => {
               placeholder=""
               defaultValue={Profiledata.age}
               readOnly
-              {...register("age")}
+              
             />
-            <small className="text-red-600">{errors.age?.message}</small>
           </div>
           <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
             <label
@@ -250,23 +206,12 @@ const Updateform = ({Profiledata}) => {
                 name=""
                 className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
                 id="grid-state"
-                {...register("role")}
+             
               >
                 <option value="employee">{Profiledata.role}</option>
               </select>
-              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg
-                  className="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                </svg>
-              </div>
             </div>
-          {errors.role && (
-            <small className="text-red-600">{errors.role.message}</small>
-          )}
+         
           </div>
           <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
             <label
@@ -280,23 +225,13 @@ const Updateform = ({Profiledata}) => {
                 name=""
                 className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
                 id="grid-state"
-                {...register("gender")}
+                
               >
                 <option value="">{Profiledata.gender}</option>
               </select>
-              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg
-                  className="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                </svg>
-              </div>
+              
             </div>
-          {errors.role && (
-            <small className="text-red-600">{errors.gender.message}</small>
-          )}
+         
           </div>
         </div>
         <div className="flex flex-wrap mx-3 mb-6 justify-center items-center">
@@ -313,9 +248,9 @@ const Updateform = ({Profiledata}) => {
               type="text"
               placeholder=""
               defaultValue={Profiledata.nationality}
-              {...register("nationality")}
+              
             />
-            <small className="text-red-600">{errors.nationality?.message}</small>
+            
           </div>
           <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
             <label
@@ -330,9 +265,8 @@ const Updateform = ({Profiledata}) => {
               type="text"
               placeholder=""
               defaultValue={Profiledata.religion}
-              {...register("religion")}
+            
             />
-            <small className="text-red-600">{errors.religion?.message}</small>
           </div>
           <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
             <label
@@ -348,9 +282,8 @@ const Updateform = ({Profiledata}) => {
               placeholder=""
               defaultValue={Date(Profiledata.joiningdate)}
               readOnly
-              {...register("joiningdate")}
+              
             />
-            <small className="text-red-600">{errors.joiningdate?.message}</small>
           </div>
           <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
             <label
@@ -365,9 +298,8 @@ const Updateform = ({Profiledata}) => {
               type="text"
               placeholder=""
               defaultValue={Profiledata.martialStatus}
-              {...register("martialStatus")}
+            
             />
-            <small className="text-red-600">{errors.martialStatus?.message}</small>
           </div>
         </div>
         <div className="flex flex-wrap mx-3 mb-6 justify-center items-center">
@@ -384,9 +316,8 @@ const Updateform = ({Profiledata}) => {
               type="text"
               placeholder=""
               defaultValue={Profiledata.emergencyContactName}
-              {...register("emergencyContactName")}
+             
             />
-            <small className="text-red-600">{errors.emergencyContactName?.message}</small>
           </div>
           <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
             <label
@@ -401,9 +332,8 @@ const Updateform = ({Profiledata}) => {
               type="text"
               placeholder=""
               defaultValue={Profiledata.emergencyContactRelationship}
-              {...register("emergencyContactRelationship")}
+            
             />
-            <small className="text-red-600">{errors.emergencyContactRelationship?.message}</small>
           </div>
           <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
             <label
@@ -418,9 +348,8 @@ const Updateform = ({Profiledata}) => {
               type="number"
               placeholder=""
               defaultValue={Profiledata.EmergencyContactNumber}
-              {...register("EmergencyContactNumber")}
+            
             />
-            <small className="text-red-600">{errors.EmergencyContactNumber?.message}</small>
           </div>
         </div>
         <div className="flex flex-wrap mx-3 mb-6 justify-center items-center">
@@ -438,9 +367,8 @@ const Updateform = ({Profiledata}) => {
               placeholder=""
               defaultValue={Profiledata.BankName}
               readOnly
-              {...register("BankName")}
+              
             />
-            <small className="text-red-600">{errors.BankName?.message}</small>
           </div>
           <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
             <label
@@ -456,9 +384,8 @@ const Updateform = ({Profiledata}) => {
               placeholder=""
               defaultValue={Profiledata.BranchName}
               readOnly
-              {...register("BranchName")}
+              
             />
-            <small className="text-red-600">{errors.BranchName?.message}</small>
           </div>
           <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
             <label
@@ -474,9 +401,9 @@ const Updateform = ({Profiledata}) => {
               placeholder=""
               defaultValue={Date(Profiledata.dob)}
               readOnly
-              {...register("dob")}
+            
             />
-            <small className="text-red-600">{errors.dob?.message}</small>
+           
           </div>
           <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
             <label
@@ -492,9 +419,9 @@ const Updateform = ({Profiledata}) => {
               placeholder=""
               defaultValue={Profiledata.IFSCcode}
               readOnly
-              {...register("IFSCcode")}
+            
             />
-            <small className="text-red-600">{errors.IFSCcode?.message}</small>
+           
           </div>
         </div>
         <div className="flex flex-wrap mx-3 mb-6 justify-center items-center">
@@ -512,9 +439,9 @@ const Updateform = ({Profiledata}) => {
               placeholder=""
               defaultValue={Profiledata.ACholdername}
               readOnly
-              {...register("ACholdername")}
+              
             />
-            <small className="text-red-600">{errors.ACholdername?.message}</small>
+           
           </div>
           <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
             <label
@@ -530,9 +457,9 @@ const Updateform = ({Profiledata}) => {
               placeholder=""
               defaultValue={Profiledata.BankAccNo}
               readOnly
-              {...register("BankAccNo")}
+             
             />
-            <small className="text-red-600">{errors.BankAccNo?.message}</small>
+           
           </div>
           <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
             <label
@@ -548,9 +475,9 @@ const Updateform = ({Profiledata}) => {
               placeholder=""
               defaultValue={Profiledata.PanNo}
               readOnly
-              {...register("PanNo")}
+            
             />
-            <small className="text-red-600">{errors.PanNo?.message}</small>
+          
           </div>
         </div>
         <div className="flex justify-end mr-6 mt-5 ">   
